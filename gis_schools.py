49b0d5647ebf66f030b2962124d009fcebe596a8
hamike <49029879+hamike@users.noreply.github.com>
2020-08-26 21:46:46 +0200

    Update gis_schools.py

--- gis_schools.py
+++ gis_schools.py
@@ -4,50 +4,64 @@ import os
 import errno
 
 def main():
-    schulen = ['a', 'b', 'c']
-    lehren = [1, 2, 3]
-    gemeinden = ['Uster', 'Rüti', 'Zürich', 'Dummy']
+    schulen = ['a', 'b', 'c'] #All schools
+    lehren = [1, 2, 3] #All study directions
+    gemeinden = ['Uster', 'Rüti', 'Zürich', 'Dummy'] #All places of living
 
-    angebot = {'a': [1, 2], 'b': [2, 3], 'c': [1, 3]}
+    angebot = {'a': [1, 2], 'b': [2, 3], 'c': [1, 3]} #What school offers what study direction
 
     plaetze = {('a', 1): 10, ('a', 2): 6, ('a', 3): 0,
                 ('b', 1): 0, ('b', 2): 10, ('b', 3): 10,
-                ('c', 1): 10, ('c', 2): 0, ('c', 3): 10}
+                ('c', 1): 10, ('c', 2): 0, ('c', 3): 10} #Number of available spaces
 
     lehrlinge = {('Uster', 1): 3, ('Uster', 2): 4, ('Uster', 3):1,
                 ('Rüti', 1): 4, ('Rüti', 2): 2, ('Rüti', 3): 6,
                 ('Zürich', 1): 3, ('Zürich', 2): 4, ('Zürich', 3): 3,
-                ('Dummy', 1): 10, ('Dummy', 2): 6, ('Dummy', 3): 10}
+                ('Dummy', 1): 10, ('Dummy', 2): 6, ('Dummy', 3): 10} #Number of students per study direction for every commune
 
     zeit = {('Uster', 'a'): 3, ('Uster', 'b'): 5, ('Uster', 'c'): 4,
             ('Rüti', 'a'): 6, ('Rüti', 'b'): 1, ('Rüti','c'): 3,
             ('Zürich', 'a'): 2, ('Zürich', 'b'): 3, ('Zürich', 'c'): 6,
-            ('Dummy', 'a'): 0, ('Dummy', 'b'): 0, ('Dummy', 'c'): 0}
+            ('Dummy', 'a'): 0, ('Dummy', 'b'): 0, ('Dummy', 'c'): 0} #Travel time between places of living and schools
 
     c = {}
     for g in gemeinden:
         for s in schulen:
             for a in angebot[s]:
-                c[g, s, a] = zeit[g, s]
+                c[g, s, a] = zeit[g, s] #Theoretically redundant but not yet fixed
 
-    zuteilung(gemeinden, schulen, lehren, c, lehrlinge, plaetze)
+    zuteilung(gemeinden, schulen, lehren, c, lehrlinge, plaetze) #Calling assignment function
 
 def zuteilung(gemeinden, schulen, lehren, c, lehrlinge, plaetze):
-    model = pulp.LpProblem(name='Schulen_Test', sense=pulp.LpMinimize)
+    '''
+    Function to run the actual transportation problem
+    
+    :param gemeinden: List of all communes [type: List]
+    :param schulen: List of all schools [type: List]
+    :param lehren: List of all study directions [type: List]
+    :param c: Dictionary of travel times between communes and schools (cost) [type: Dictionary]
+    :param lehrlinge: Dictionary of students per commune and study direction [type: Dictionary]
+    :param plaetze: Dictionary of spaces availabel per school and study direction [type: Dictionary]
+    
+    :return: None
+    '''
+    model = pulp.LpProblem(name='Schulen_Test', sense=pulp.LpMinimize) #Creating model
     
     x = {}
-    for g,s,l in c:
-        x[g,s,l] = pulp.LpVariable(name='x[{}, {}, {}]'.format(g,s,l), lowBound=0, cat=pulp.LpInteger)
+    for g,s,l in c: #Creating decision variables
+        x[g,s,l] = pulp.LpVariable(name='x[{}, {}, {}]'.format(g,s,l), lowBound=0, cat=pulp.LpInteger) #Constraint (4) included
     
-    for g in gemeinden: #Lehrbetriebe
-        for l in lehren: #Berufsgattungen
+    #Constraint (2)
+    for g in gemeinden:
+        for l in lehren:
             model.addConstraint(pulp.LpConstraint(
                 e=pulp.lpSum(x[g,s,l] for s in schulen if (g,s,l) in x),
                 sense=pulp.LpConstraintEQ,
                 name='Demand[{}, {}]'.format(g,l),
                 rhs=lehrlinge[g,l]))
     
-    for s in schulen: #Schulen
+    #Constraint (3)
+    for s in schulen:
         for l in lehren:
             model.addConstraint(pulp.LpConstraint(
                 e=pulp.lpSum(x[g,s,l] for g in gemeinden if (g,s,l) in x),
@@ -56,6 +70,7 @@ def zuteilung(gemeinden, schulen, lehren, c, lehrlinge, plaetze):
                 rhs=plaetze[s, l]
             ))
     
+    #Constraint(5)
     for g in gemeinden:
         for l in lehren:
             model.addConstraint(pulp.LpConstraint(
@@ -66,22 +81,22 @@ def zuteilung(gemeinden, schulen, lehren, c, lehrlinge, plaetze):
             ))
     
     
-    model.setObjective(pulp.lpSum(c[g,s,l] * x[g,s,l] for g,s,l in x))
+    model.setObjective(pulp.lpSum(c[g,s,l] * x[g,s,l] for g,s,l in x)) #Objective function
 
     print('Writing lp file')
     model.writeLP(model.name + '.lp')
 
     print('Starting optimization')
     model.solve()
-    #print(model.constraints['Unique_assignments_Uster,_1_'])
-    print(model.constraints)
     print(pulp.LpStatus[model.status])
 
-
+    #---------DEBUGGING------------#
     for g,s,l in x:
         if x[g,s,l].varValue != 0:
-            print("{:10g} Schüler von {} aus Gemeinde {} an Schule {}".format(x[g,s,l].varValue, l, g, s))
-
+            print("{:10g} Schüler von {} aus Gemeinde {} an Schule {}".format(x[g,s,l].varValue, l, g, s)) #Print results for debugging purposes
+    #------------------------------#
+    
+    #Export results to CSV
     output = []
     for g,s,l in x:
         var_output = {
@@ -97,14 +112,28 @@ def zuteilung(gemeinden, schulen, lehren, c, lehrlinge, plaetze):
     return
 
 def write_to_csv(output_df, output_name='output', output_folder='output'):
+    '''
+    Helper function to export data to  CSV
+    
+    :param output_df: Dataframe to be exported [type: pandas DataFrame]
+    :param output_name: Name of output file [type: String]
+    :param output_folder: Path of the output file [type: String]
+    
+    CREDIT: Ehsan Khodabandeh
+    '''
     output_dir = get_file_directory(output_folder + '/')
     ensure_directory_exists(output_dir)
     out_name = ''.join((output_name, '.csv'))
     output_df.to_csv(os.path.join(output_dir, out_name), encoding='latin-1')
 
 def ensure_directory_exists(directory):
-    # Create the 'directory' if it doesn't exists. If it exists,
-    # it does nothing unless the error code is something else!
+    '''
+    Creates directory if it doesn't exist yet. If it exists if does nothing unless the error is something else.
+    
+    :param directory: Directory to be created [type: String]
+    
+    CREDIT: Ehsan Khodabandeh
+    '''
     try:
         os.makedirs(directory)
     except OSError as e:
@@ -112,6 +141,13 @@ def ensure_directory_exists(directory):
             raise
 
 def get_file_directory(file):
+    '''
+    Get directory of file
+    
+    :param file: File to be found [type: String]
+    
+    CREDIT: Ehsan Khodabandeh
+    '''
     try:
         script_path = os.path.dirname(__file__)
     except NameError:
